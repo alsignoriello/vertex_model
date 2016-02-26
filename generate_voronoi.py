95fdb6b81e503b32e8ff4a04f35c7bbb7c2ae2cc
as2565 <alexandra.signoriello@yale.edu>
2016-02-26 15:03:08 -0500

    Voronoi with periodic bounds. Need to add build cell cycles

--- generate_voronoi.py
+++ generate_voronoi.py
@@ -2,7 +2,7 @@
 import numpy as np 
 from scipy.spatial import Voronoi
 import matplotlib.pyplot as plt
-from geometry import periodic_diff, euclidean_distance
+
 
 """
 
@@ -16,18 +16,20 @@ date: 2/15/16
 """
 
 
-def plot_vertices(vertices):
-	count = 0
+def plot_bounds(x_min, x_max, y_min, y_max, color):
+	plt.plot([x_min,x_max], [y_min,y_min], color=color)
+	plt.plot([x_min,x_min], [y_min,y_max], color=color)
+	plt.plot([x_max,x_max], [y_min,y_max], color=color)
+	plt.plot([x_max,x_min], [y_max,y_max], color=color)
+	return
+
+def plot_vertices(vertices, color):
 	for x,y in vertices:
-		plt.scatter(x,y,color="b")
-		if x >= 0 and y >= 0:
-			if x <= 1 and y <=1:
-				count += 1
-	print count
+		plt.scatter(x,y,color=color)
 	return 
 
-def plot_voronoi(edges, filename, color):
-	# plt.cla()
+# edges correspond to indices of vertices
+def plot_voronoi(vertices, edges, color):
 	for edge in edges:
 		e1 = edge[0]
 		e2 = edge[1]
@@ -36,20 +38,125 @@ def plot_voronoi(edges, filename, color):
 			x1,y1 = vertices[edge[0]]
 			x2,y2 = vertices[edge[1]]
 			plt.plot([x1,x2],[y1,y2],color=color)
-	
-		else:
-			if e1 != -1:
-				x,y = vertices[e1]
-				plt.scatter(x,y,color="r")
-			else:
-				x,y = vertices[e2]
-				plt.scatter(x,y,color="r")
-	
-	plt.axis([-0.5,1.5,-0.5,1.5])
-	# plt.savefig(filename)
-	# plt.close()
+
 	return
 
+
+# get vertices with respect to periodic boundaries
+def get_vertices(vertices, x_min, x_max, y_min, y_max):
+	count = 0
+	v = []
+	index_map = {}
+	for i,(x,y) in enumerate(vertices):
+		if x >= x_min and y >= y_min:
+			if x <= x_max and y <= y_max:
+				index_map[i] = len(v)
+				v.append((x,y))
+				count += 1
+	print "There are %d vertices\n" % count 
+
+	return np.array(v), index_map
+
+# get edges with respect to periodic boundaries
+# vertices and edges are from voronoi with tiled coordinates
+def get_edges(vertices, edges, index_map, L):
+	e = []
+	for edge in edges:
+		i1 = edge[0]
+		i2 = edge[1]
+
+		# Case 1: i1 and i2 in index map
+		# Append indices for vertex list wrt periodic bounds
+		if i1 in index_map and i2 in index_map:
+			e.append((index_map[i1],index_map[i2]))
+
+		# Case 2: neither in index map
+		# Do nothing
+
+		# Case 3: i1 or i2 in index map
+		# 		  but not both 
+		# Because of periodicity, this edge needs to have a 
+		# connecting edge that wraps around
+		if i1 in index_map and i2 not in index_map:
+			# find the vertex that it "wraps around" to
+			# v = vertex in plane
+			v = np.array(vertices[i1])
+			# v1 = vertex out of plane
+			v1 = np.array(vertices[i2])
+
+			# print v1
+			if v1[0] < 0:
+				v1[0] = 1 + v1[0]
+
+			if v1[0] > L[0]:
+				v1[0] = v1[0] - 1
+
+			if v1[1] < 0:
+				v1[1] = 1 + v1[1]
+
+			if v1[1] > L[0]:
+				v1[1] = v1[1] - 1
+
+			# print v, v1
+
+			# # # find index of this vertex 
+			for key in index_map:
+				if abs(vertices[key][0] - v1[0]) < 10**-6:
+					e.append((index_map[i1], index_map[key]))
+					# print "yes"
+
+
+		# include this IF you want duplicate edges...
+		# if i1 not in index_map and i2 in index_map:
+		# 	# find the vertex that it "wraps around" to
+		# 	# v = vertex in plane
+		# 	v = np.array(vertices[i2])
+		# 	# v1 = vertex out of plane
+		# 	v1 = np.array(vertices[i1])
+
+
+		# 	# print v1
+		# 	if v1[0] < 0:
+		# 		v1[0] = 1 + v1[0]
+
+		# 	if v1[0] > L[0]:
+		# 		v1[0] = v1[0] - 1
+
+		# 	if v1[1] < 0:
+		# 		v1[1] = 1 + v1[1]
+
+		# 	if v1[1] > L[0]:
+		# 		v1[1] = v1[1] - 1
+
+		# 	# print v, v1
+
+		# 	# # find index of this vertex 
+		# 	for key in index_map:
+		# 		if abs(vertices[key][0] - v1[0]) < 10**-6:
+		# 			e.append((index_map[i2], index_map[key]))
+		# 			# print "yes"
+
+
+	return e
+
+
+# get cells
+def get_cells(vertices, edges):
+	
+	cells = {}
+
+
+	# cycle through cells
+	for i,vertex in enumerate(vertices):
+
+		# find edges associated with this vertex
+		for edge in edges:
+			
+
+
+
+
+	return cells
 
 
 # Assumes coordiantes are between 0 and 1 
@@ -97,74 +204,106 @@ def tile_coordinates(coords, N):
 
 
 
+np.random.seed([1938420])
+
 # number of points
 N = 48
 
+x_min = 0.
+x_max = 1.
+y_min = 0.
+y_max = 1.
 
-# L = length of box
-lx = 1.
-ly = 1.
+L = np.array([x_max, y_max])
 
 # generate random coordinates between 0 and 1
-coords = np.random.uniform(0,1,size=(N,2))
-
+coords = np.random.uniform(x_min, x_max, size=(N,2))
 
 # To get a configuration that works with periodic boundaries,
 # Make a tile 3 x 3 tile with same coordinates
 coord_tile = tile_coordinates(coords, N)
-# plt.scatter(coord_tile[:,0],coord_tile[:,1])
-# plt.show()
 
-
-# generate voronoi diagram
-# vor = Voronoi(coords)
+# generate voronoi diagram for tiled boxes
 vor = Voronoi(coord_tile)
 
-# vertices corresponding to voronoi cells
-vertices = vor.vertices
-plot_vertices(vertices)
-edges =  vor.ridge_vertices
-plot_voronoi(edges, "voronoi_tile.jpg", "k")
+
+tile_vertices = vor.vertices
+plot_vertices(tile_vertices, "k")
+
+tile_edges = vor.ridge_vertices
+plot_voronoi(tile_vertices, tile_edges, "k")
+
+# get vertices for cells in center tile
+vertices, index_map = get_vertices(tile_vertices, x_min, x_max, y_min, y_max)
+plot_vertices(vertices, "c")
+
+# plot boundaries
+plot_bounds(x_min, x_max, y_min, y_max, "m")
+plt.axis([-0.5,1.5,-0.5,1.5])
+
+# for key in index_map:
+# 	print key, index_map[key]
 
 
-# generate voronoi diagram
+edges = get_edges(tile_vertices, tile_edges, index_map, L)
+# plotting routine does not understand periodic boundaries currently...
+# plot_voronoi(vertices, edges, "c")
+# plt.savefig("voronoi_periodic.jpg")
+
+cells = get_cells(vertices, edges)
+
+# ADD CHECK TO ASSERT CYCLES ARE IN COUNTER CLOCKWISE ORDER!!
+
+
+
+# Write data to text files
+
+# write vertices
+np.savetxt("voronoi_vertices.txt", vertices)
+
+
+# write edges
+np.savetxt("voronoi_edges.txt", edges, fmt="%d")
+
+# write cells
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # generate voronoi diagram
+# # vor = Voronoi(coords)
 # vor = Voronoi(coords)
-vor = Voronoi(coords)
 
+# # vertices corresponding to voronoi cells
+# vertices = vor.vertices
+# edges =  vor.ridge_vertices
+# plot_voronoi(vertices, edges, "voronoi.jpg", "c")
 
-# vertices corresponding to voronoi cells
-vertices = vor.vertices
-edges =  vor.ridge_vertices
-plot_voronoi(edges, "voronoi.jpg", "c")
-
-
-# plot lines for axes
-plt.plot([0,1], [0,0], color="k")
-plt.plot([0,0], [0,1], color="k")
-plt.plot([1,1], [0,1], color="k")
-plt.plot([1,0], [1,1], color="k")
-
-plt.savefig("voronoi_test.jpg")
-
-
-
-
-
-# export vertices
-
-# export cells
-
-# export edges
-
-
-
-
-
-
-
-
-
-
+# plt.savefig("voronoi_test.jpg")
 
 # # scale to box length
 # vertices[:,0] = (vertices[:,0] - np.min(vertices[:,0])) / (np.max(vertices[:,0]) - np.min(vertices[:,0]))
